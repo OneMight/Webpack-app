c2f5a6872f8cd2e2feb86ff081455e20a445a2e1
OneMight <dimamirankov63@gmail.com>
2025-12-10 17:20:50 +0300

    feat: add timer 3 seconds to automatic closing alert menu

--- src/components/alert/alert.tsx
+++ src/components/alert/alert.tsx
@@ -1,13 +1,24 @@
 import { IAlert } from "../../types/intefaces";
 import { Alert, Box, IconButton, Collapse } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export const AlertMui = ({ children, setError }: IAlert) => {
   const [open, setOpen] = useState<boolean>(true);
+
   const handleCloseAlert = () => {
     setOpen(false);
     setError(null);
   };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setOpen(false);
+      setError(null);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
   return (
     <Box sx={{ position: "fixed", top: "80px", zIndex: 10 }}>
       <Collapse in={open}>
